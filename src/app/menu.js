cf75a489b37dd72a08b22a0aaa45534006e4cf8f
wenzhenghaha <wenzhenghaha@163.com>
2020-10-15 17:13:26 +0800

    add the scroll menu

--- src/app/menu.js
+++ src/app/menu.js
@@ -38,51 +38,54 @@ function Menu({menuList:list , type}){
                     <div className='shrink' onClick={() => {
                         setFull(!isFull);
                     }}></div>
-                    <div className='logo'></div>
-                    {
-                        list.map((item, index) => {
-                            setChecked(item , props.location);
-                            return (
-                                <div className='menu-group' style={{ height: item.open && isFull ? `${48 + item.list.length * 40}px` : '48px' }} key={index} onClick={() => {
-                                    item.open = !item.open;
-                                    setRefresh(++refresh);
-                                }}>
+                    <div className='menu-scroll' style={{width: isFull ? '240px' : '80px' }}>
+                        <div className='menu-list' >
+                            <div className='logo'></div>
+                            {
+                                list.map((item, index) => {
+                                    setChecked(item , props.location);
+                                    return (
+                                        <div className='menu-group' style={{ height: item.open && isFull ? `${48 + item.list.length * 40}px` : '48px' }} key={index} onClick={() => {
+                                            item.open = !item.open;
+                                            setRefresh(++refresh);
+                                        }}>
 
-                                    <div className={`main-title ${item.checked || (item.subChecked && !isFull) ? 'checked' : ''}`} onClick={() => {
-                                        if (!(item.list && item.list.length > 0)){
-                                            lib.openPage(item.url);
-                                        }
-                                    }}>
-                                        <span className='icon' dangerouslySetInnerHTML={{ __html: item.icon }}></span>
-                                        {item.title}
-                                        {item.list.length > 0 && <div className={`icon-down ${item.open ? 'open' : ''}`} >&#xe6c7;</div>}
-                                    </div>
+                                            <div className={`main-title ${item.checked || (item.subChecked && !isFull) ? 'checked' : ''}`} onClick={() => {
+                                                if (!(item.list && item.list.length > 0)){
+                                                    lib.openPage(item.url);
+                                                }
+                                            }}>
+                                                <span className='icon' dangerouslySetInnerHTML={{ __html: item.icon }}></span>
+                                                {item.title}
+                                                {item.list.length > 0 && <div className={`icon-down ${item.open ? 'open' : ''}`} >&#xe6c7;</div>}
+                                            </div>
 
-                                    <div className='sub-box' onClick={(e) => { e.stopPropagation()}}>
-                                        <div className='sub-list'>
-                                            {item.list && item.list.length > 0 ? 
-                                                item.list.map((sub, key) =>
-                                                    <Fragment key={key}>
-                                                        <div className={sub.checked && isFull ? 'sub-title checked' : 'sub-title'} 
+                                            <div className='sub-box' onClick={(e) => { e.stopPropagation()}}>
+                                                <div className='sub-list'>
+                                                    {item.list && item.list.length > 0 ? 
+                                                        item.list.map((sub, key) =>
+                                                            <Fragment key={key}>
+                                                                <div className={sub.checked && isFull ? 'sub-title checked' : 'sub-title'} 
+                                                                onClick={() => lib.openPage(sub.url)}>
+                                                                    {sub.title}
+                                                                </div>    
+                                                            </Fragment>
+                                                        )
+                                                        :
+                                                        !isFull && type == 'sub-menu' &&
+                                                        <div className={item.checked ? 'sub-title checked' : 'sub-title'} 
                                                         onClick={() => lib.openPage(sub.url)}>
-                                                            {sub.title}
-                                                        </div>    
-                                                    </Fragment>
-                                                )
-                                                :
-                                                !isFull && type == 'sub-menu' &&
-                                                <div className={item.checked ? 'sub-title checked' : 'sub-title'} 
-                                                onClick={() => lib.openPage(sub.url)}>
-                                                    {item.title}
+                                                            {item.title}
+                                                        </div>
+                                                    }
                                                 </div>
-                                            }
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                    
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
             )
         }}
